e4d6aadd7cdb5ef9adc33bb17706be4eb68afa84
T <carando@ubiqtek.biz>
2016-05-21 15:47:39 -0500

    Added javadoc Rearranged some statements Changed constructor, getInstance to generate the MovieDBHelper instance that is locally stored.

--- app/src/main/java/com/ubiq/android/app/mobilemovies/data/MovieOToRMapper.java
+++ app/src/main/java/com/ubiq/android/app/mobilemovies/data/MovieOToRMapper.java
@@ -1,6 +1,7 @@
 package com.ubiq.android.app.mobilemovies.data;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
@@ -17,23 +18,42 @@ import java.util.List;
  * Created by T on 3/28/2016.
  */
 public class MovieOToRMapper {
-
-    public  static long   MOVIE_IN_FAVORITES = -2;
     private static String LOG_TAG = MovieOToRMapper.class.getSimpleName();
+
+    /**
+     * The database location for the Movie in favorites.
+     */
+    public  static long   MOVIE_IN_FAVORITES = -2;
+
 
     private static MovieOToRMapper mMovieOToRMapper = null;
     private static MovieDBHelper   dbHelper         = null;
 
-    private MovieOToRMapper(MovieDBHelper dbHelper) {
-        this.dbHelper = dbHelper;
+    private MovieOToRMapper(Context context) {
+        dbHelper = new MovieDBHelper(context);
+        dbHelper.getWritableDatabase();
     }
 
-    public static MovieOToRMapper getInstance(MovieDBHelper dbHelper) {
+    /**
+     * Gets instance.
+     *
+     * @param context the context
+     * @return the instance
+     */
+
+    public static MovieOToRMapper getInstance(Context context) {
         if (mMovieOToRMapper == null) {
-            mMovieOToRMapper = new MovieOToRMapper(dbHelper);
+            mMovieOToRMapper = new MovieOToRMapper(context);
         }
         return mMovieOToRMapper;
     }
+
+
+    /**
+     * Gets all favorites.
+     *
+     * @return the all favorites
+     */
     public List<Movie> getAllFavorites () {
         ArrayList <Movie> favoriteMovies = null;
         Log.v(LOG_TAG, "***entered getAllFavorites");
@@ -67,13 +87,20 @@ public class MovieOToRMapper {
             String path        = cursor.getString (7);
             Log.d (LOG_TAG, "*** movie title: " + title);
             Movie movie = new Movie (id,title,rating,year,description,null,path);
+            movie.setPosterImage(image);
             favoriteMovies.add(movie);
-            rowsToRead = cursor.moveToNext();
+            rowsToRead  = cursor.moveToNext();
         }
-
+        db.close();
         return favoriteMovies;
     }
 
+    /**
+     * Insert movie into database if not there already.
+     *
+     * @param aMovie the movie
+     * @return long location of movie in database
+     */
     public long insertMovie(Movie aMovie) {
         Log.v(LOG_TAG, "***insert movie " + aMovie.toString());
         if (movieInFavorites(aMovie)) {
@@ -81,18 +108,28 @@ public class MovieOToRMapper {
         }
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = getValues (aMovie);
-        long result = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
-        return result;
+        return db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+
       }
 
+    /**
+     * Insert movie into database if not there already.
+     *
+     * @param values the content values for the movie
+     * @return long location of movie in database
+     */
     public long insertMovie (ContentValues values) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        long result = db.insert (MovieContract.MovieEntry.TABLE_NAME, null, values);
-        return result;
+        return db.insert (MovieContract.MovieEntry.TABLE_NAME, null, values);
     }
 
 
-
+    /**
+     * Returns true of the movie is already in the database
+     *
+     * @param aMovie the movie
+     * @return boolean
+     */
     private boolean movieInFavorites(Movie aMovie) {
         Log.v (LOG_TAG, "***movieInFavorites");
         String rawQueryString = "SELECT " + MovieContract.MovieEntry.COLUMN_MOVIE_TITLE +
@@ -104,9 +141,15 @@ public class MovieOToRMapper {
 
         boolean foundMovie = cursor.moveToFirst();
         Log.v (LOG_TAG, "***movieInFavorites cursor isEmpty " + foundMovie);
+        cursor.close();
         return foundMovie;
     }
-
+    /**
+     * Generates the content values for a movie
+     *
+     * @param aMovie the movie
+     * @return ContentValues
+     */
     private ContentValues getValues (Movie aMovie) {
         ContentValues values = new ContentValues();
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_ID,           aMovie.getId());
@@ -117,7 +160,7 @@ public class MovieOToRMapper {
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_RUNNING_TIME, aMovie.getRunningTime());
         values.put (MovieContract.MovieEntry.COLUMN_POSTER_PATH,        aMovie.getPosterPath());
 
-        byte[] moviePoster  = null;
+        byte[] moviePoster;
         try {
             URL  moviePosterURL = Utils.buildMovieImageURL(aMovie.getPosterPath());
             Log.v (LOG_TAG, "***moviePosterURL " + moviePosterURL);
