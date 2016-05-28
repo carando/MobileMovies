a876809b10cddd971d3f0fe9331665952c736aba
T <carando@ubiqtek.biz>
2016-05-28 18:13:51 -0500

    First

---
+++ app/src/main/java/com/ubiq/android/app/mobilemovies/callbacks/ViewInvalidCallback.java
@@ -0,0 +1,9 @@
+package com.ubiq.android.app.mobilemovies.callbacks;
+
+/**
+ * Interface for call back to clear an invalid detail movie view
+ */
+public interface ViewInvalidCallback {
+
+    void onDetailViewInvalidated () ;
+}
