e58303b1c89493e45c74242c80ecfd411a81ca93
T <carando@ubiqtek.biz>
2016-05-28 18:48:21 -0500

    First

---
+++ app/src/main/java/com/ubiq/android/app/mobilemovies/callbacks/ItemSelectedCallback.java
@@ -0,0 +1,10 @@
+package com.ubiq.android.app.mobilemovies.callbacks;
+
+/**
+ * Interface for call back to indicate a movie has been selected for
+ * detailed viewing.
+ */
+public interface ItemSelectedCallback {
+        void onItemSelected(int position);
+}
+
