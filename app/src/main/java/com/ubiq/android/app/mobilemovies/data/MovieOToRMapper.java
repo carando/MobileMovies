459b3272742a27396e5628bdc720ef77c6320b0b
T <carando@ubiqtek.biz>
2016-05-29 17:26:46 -0500

    Refactored; added functionality to store trailers and reviews in DB

--- app/src/main/java/com/ubiq/android/app/mobilemovies/data/MovieOToRMapper.java
+++ app/src/main/java/com/ubiq/android/app/mobilemovies/data/MovieOToRMapper.java
@@ -4,9 +4,12 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.util.Log;
 
 import com.ubiq.android.app.mobilemovies.model.Movie;
+import com.ubiq.android.app.mobilemovies.model.MovieReview;
+import com.ubiq.android.app.mobilemovies.model.MovieTrailer;
 import com.ubiq.android.app.mobilemovies.utils.Utils;
 
 import java.net.MalformedURLException;
@@ -15,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by T on 3/28/2016.
+ * Maps Movie, Trailer, Reviews object-to-relational and back
  */
 public class MovieOToRMapper {
     private static String LOG_TAG = MovieOToRMapper.class.getSimpleName();
@@ -55,41 +58,15 @@ public class MovieOToRMapper {
      * @return the all favorites
      */
     public List<Movie> getAllFavorites () {
-        ArrayList <Movie> favoriteMovies = null;
-        Log.v(LOG_TAG, "***entered getAllFavorites");
-        String rawQueryString = "SELECT " + MovieContract.MovieEntry.COLUMN_MOVIE_ID           + "," +
-                                            MovieContract.MovieEntry.COLUMN_MOVIE_TITLE        + "," +
-                                            MovieContract.MovieEntry.COLUMN_MOVIE_DESCRIPTION  + "," +
-                                            MovieContract.MovieEntry.COLUMN_MOVIE_RATING       + "," +
-                                            MovieContract.MovieEntry.COLUMN_MOVIE_RELEASE_YEAR + "," +
-                                            MovieContract.MovieEntry.COLUMN_MOVIE_RUNNING_TIME + "," +
-                                            MovieContract.MovieEntry.COLUMN_POSTER_IMAGE       + "," +
-                                            MovieContract.MovieEntry.COLUMN_POSTER_PATH        +
-                " FROM  " + MovieContract.MovieEntry.TABLE_NAME ;
-        Log.v (LOG_TAG, "query: " + rawQueryString);
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor cursor =  db.rawQuery(rawQueryString, null);
+          List <Movie> favoriteMovies;
+          SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        boolean rowsToRead = cursor.moveToFirst();
-        if (rowsToRead) {
-            favoriteMovies = new ArrayList<Movie>();
-            Log.v (LOG_TAG, "***Movie titles in Favorites");
-        }
-
-        while (rowsToRead) {
-            int    id          = cursor.getInt    (0);
-            String title       = cursor.getString (1);
-            String description = cursor.getString (2);
-            String rating      = cursor.getString (3);
-            String year        = cursor.getString (4);
-            String runningTime = cursor.getString (5);
-            byte[] image       = cursor.getBlob   (6);
-            String path        = cursor.getString (7);
-            Log.d (LOG_TAG, "*** movie title: " + title);
-            Movie movie = new Movie (id,title,rating,year,description,null,path);
-            movie.setPosterImage(image);
-            favoriteMovies.add(movie);
-            rowsToRead  = cursor.moveToNext();
+        favoriteMovies = getMovies(db);
+        for (int i=0; i<favoriteMovies.size(); i++) {
+            Movie aMovie = favoriteMovies.get(i);
+            getTrailers(db, aMovie);
+            getReviews (db, aMovie);
+            aMovie.setMovieDetailLoaded(true);
         }
         db.close();
         return favoriteMovies;
@@ -102,14 +79,42 @@ public class MovieOToRMapper {
      * @return long location of movie in database
      */
     public long insertMovie(Movie aMovie) {
+        final int   INSERT_FAILED = -1;
+        long        insertResult  = INSERT_FAILED;
         Log.v(LOG_TAG, "***insert movie " + aMovie.toString());
+
         if (movieInFavorites(aMovie)) {
             return MOVIE_IN_FAVORITES;
         }
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        ContentValues values = getValues (aMovie);
-        return db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
-
+        ContentValues values = getContentValues(aMovie);
+        try {
+            db.beginTransaction();
+            insertResult = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+            if (insertResult == INSERT_FAILED) {
+                db.endTransaction();
+                db.close();
+                return INSERT_FAILED;
+            }
+            boolean success = insertTrailers(aMovie);
+            if (!success) {
+                db.endTransaction();
+                db.close();
+                return INSERT_FAILED;
+            }
+            success = insertReview(aMovie);
+            if (!success) {
+                db.endTransaction();
+                db.close();
+                return INSERT_FAILED;
+            }
+            db.setTransactionSuccessful();
+            db.endTransaction();
+            db.close();
+        } catch (Exception e) {
+            Log.e (LOG_TAG, "***" + e.toString());
+        }
+        return insertResult;
       }
 
     /**
@@ -130,7 +135,7 @@ public class MovieOToRMapper {
      * @param aMovie the movie
      * @return boolean
      */
-    private boolean movieInFavorites(Movie aMovie) {
+    public boolean movieInFavorites(Movie aMovie) {
         Log.v (LOG_TAG, "***movieInFavorites");
         String rawQueryString = "SELECT " + MovieContract.MovieEntry.COLUMN_MOVIE_TITLE +
                 " FROM  " + MovieContract.MovieEntry.TABLE_NAME      +
@@ -150,7 +155,7 @@ public class MovieOToRMapper {
      * @param aMovie the movie
      * @return ContentValues
      */
-    private ContentValues getValues (Movie aMovie) {
+    private ContentValues getContentValues(Movie aMovie) {
         ContentValues values = new ContentValues();
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_ID,           aMovie.getId());
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_TITLE,        aMovie.getTitle());
@@ -159,13 +164,19 @@ public class MovieOToRMapper {
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_RATING,       aMovie.getRating());
         values.put (MovieContract.MovieEntry.COLUMN_MOVIE_RUNNING_TIME, aMovie.getRunningTime());
         values.put (MovieContract.MovieEntry.COLUMN_POSTER_PATH,        aMovie.getPosterPath());
+        values.put (MovieContract.MovieEntry.COLUMN_POSTER_FILE,        aMovie.getImageFile());
 
         byte[] moviePoster;
         try {
             URL  moviePosterURL = Utils.buildMovieImageURL(aMovie.getPosterPath());
             Log.v (LOG_TAG, "***moviePosterURL " + moviePosterURL);
             moviePoster = Utils.downloadMovieImage(moviePosterURL);
-            values.put (MovieContract.MovieEntry.COLUMN_POSTER_IMAGE, moviePoster);
+            // TODO can we do a direct conversion of a movie image to a bitmap?
+            Bitmap bitmap = Utils.convertByteArrayToBitmap(moviePoster);
+            // need to save the image to a file and store the file name in the database
+            String baseFileName = Utils.createBitmapFileName();
+            String bitmapFileName = Utils.saveBitmapToFile(bitmap,baseFileName);
+            values.put (MovieContract.MovieEntry.COLUMN_POSTER_FILE, bitmapFileName);
         }
         catch (MalformedURLException e) {
             Log.e (LOG_TAG, "***" + e.toString());
@@ -174,5 +185,139 @@ public class MovieOToRMapper {
         return values;
     }
 
+    private ContentValues getContentValues (int movieID, MovieTrailer aTrailer) {
+        ContentValues values = new ContentValues();
+        values.put (MovieContract.MovieTrailers.COLUMN_MOVIE_ID, movieID);
+        values.put (MovieContract.MovieTrailers.COLUMN_TRAILER_NAME, aTrailer.getName());
+        values.put (MovieContract.MovieTrailers.COLUMN_TRAILER_LOCATION, aTrailer.getYoutubeSource());
+        return values;
+    }
 
+    private ContentValues getContentValues (int movieID, MovieReview aReview) {
+        ContentValues values = new ContentValues();
+        values.put (MovieContract.MovieReviews.COLUMN_MOVIE_ID, movieID);
+        values.put (MovieContract.MovieReviews.COLUMN_REVIEWER_NAME, aReview.getAuthor());
+        values.put (MovieContract.MovieReviews.COLUMN_REVIEW_TEXT, aReview.getReview());
+        return values;
+    }
+
+    private boolean insertTrailers (Movie aMovie) {
+        final int   INSERT_FAILED = -1;
+        List <MovieTrailer> trailers = aMovie.getMovieTrailers();
+        int size = trailers.size();
+        if (size == 0) return true;
+
+        int movieID = aMovie.getId();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        for (int i=0; i<size; i++) {
+            ContentValues values = getContentValues(movieID,trailers.get(i));
+            long result = db.insert(MovieContract.MovieTrailers.TABLE_NAME, null, values);
+            if (result == INSERT_FAILED) return false;
+        }
+        return true;
+    }
+
+
+    private boolean insertReview (Movie aMovie) {
+        final int   INSERT_FAILED = -1;
+        List <MovieReview> reviews = aMovie.getMovieReviews();
+        int size = reviews.size();
+        if (size == 0) return true;
+
+        int movieID = aMovie.getId();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        for (int i=0; i<size; i++) {
+            ContentValues values = getContentValues(movieID,reviews.get(i));
+            long result = db.insert(MovieContract.MovieReviews.TABLE_NAME, null, values);
+            if (result == INSERT_FAILED) return false;
+        }
+        return true;
+    }
+
+    private List <Movie> getMovies (SQLiteDatabase db) {
+        ArrayList <Movie> favoriteMovies = null;
+        Log.v(LOG_TAG, "***entered getMovies");
+        String rawQueryString = "SELECT " + MovieContract.MovieEntry.COLUMN_MOVIE_ID           + "," +
+                MovieContract.MovieEntry.COLUMN_MOVIE_TITLE        + "," +
+                MovieContract.MovieEntry.COLUMN_MOVIE_DESCRIPTION  + "," +
+                MovieContract.MovieEntry.COLUMN_MOVIE_RATING       + "," +
+                MovieContract.MovieEntry.COLUMN_MOVIE_RELEASE_YEAR + "," +
+                MovieContract.MovieEntry.COLUMN_MOVIE_RUNNING_TIME + "," +
+                MovieContract.MovieEntry.COLUMN_POSTER_PATH        + "," +
+                MovieContract.MovieEntry.COLUMN_POSTER_FILE        +
+                " FROM  " + MovieContract.MovieEntry.TABLE_NAME ;
+        Log.v (LOG_TAG, "query: " + rawQueryString);
+        Cursor cursor =  db.rawQuery(rawQueryString, null);
+
+        boolean rowsToRead = cursor.moveToFirst();
+        if (rowsToRead) {
+            favoriteMovies = new ArrayList<Movie>();
+            Log.v (LOG_TAG, "***Movie titles in Favorites");
+        }
+
+        while (rowsToRead) {
+            int    id          = cursor.getInt    (0);
+            String title       = cursor.getString (1);
+            String description = cursor.getString (2);
+            String rating      = cursor.getString (3);
+            String year        = cursor.getString (4);
+            String runningTime = cursor.getString (5);
+            String path        = cursor.getString (6);
+            String imageFile   = cursor.getString (7);
+            Log.d (LOG_TAG, "*** movie title: " + title);
+            Movie movie = new Movie (id,title,rating,year,description,path);
+            movie.setFavorite(true);
+            favoriteMovies.add(movie);
+            rowsToRead  = cursor.moveToNext();
+        }
+        cursor.close();
+        return favoriteMovies;
+    }
+
+    private void getTrailers (SQLiteDatabase db, Movie aMovie) {
+        int movieID = aMovie.getId();
+        String rawQueryString = "SELECT " +
+                MovieContract.MovieTrailers.COLUMN_TRAILER_NAME        + ", " +
+                MovieContract.MovieTrailers.COLUMN_TRAILER_LOCATION    + "  " +
+                "FROM "  + MovieContract.MovieTrailers.TABLE_NAME      + "  " +
+                "WHERE " + MovieContract.MovieTrailers.COLUMN_MOVIE_ID + " = " +
+                movieID;
+
+        Log.v (LOG_TAG, "query: " + rawQueryString);
+
+        Cursor cursor =  db.rawQuery(rawQueryString, null);
+        boolean rowsToRead = cursor.moveToFirst();
+        while (rowsToRead) {
+            String trailerName     = cursor.getString(0);
+            String trailerLocation = cursor.getString(1);
+            MovieTrailer trailer = new MovieTrailer(trailerName,trailerLocation);
+            aMovie.addTrailer(trailer);
+            rowsToRead = cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+    private void getReviews (SQLiteDatabase db, Movie aMovie) {
+        int movieID = aMovie.getId();
+        String rawQueryString = "SELECT " +
+                MovieContract.MovieReviews.COLUMN_REVIEWER_NAME       + ", " +
+                MovieContract.MovieReviews.COLUMN_REVIEW_TEXT         + "  " +
+                "FROM "  + MovieContract.MovieReviews.TABLE_NAME      + "  " +
+                "WHERE " + MovieContract.MovieReviews.COLUMN_MOVIE_ID + " = " +
+                movieID;
+
+        Log.v (LOG_TAG, "query: " + rawQueryString);
+
+        Cursor cursor =  db.rawQuery(rawQueryString, null);
+        boolean rowsToRead = cursor.moveToFirst();
+        int     id = 0;
+        while (rowsToRead) {
+            String reviewName  = cursor.getString(0);
+            String reviewText  = cursor.getString(1);
+            MovieReview review = new MovieReview(String.valueOf(id++),reviewName,reviewText);
+            aMovie.addReview(review);
+            rowsToRead = cursor.moveToNext();
+        }
+        cursor.close();
+    }
 }
