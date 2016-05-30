7b046a2c6ac6568d57482e311326f11d7503de3d
T <carando@ubiqtek.biz>
2016-05-30 18:44:45 -0500

    code cleanup

--- app/src/main/java/com/ubiq/android/app/mobilemovies/activities/MovieReviewsActivity.java
+++ app/src/main/java/com/ubiq/android/app/mobilemovies/activities/MovieReviewsActivity.java
@@ -1,7 +1,6 @@
 package com.ubiq.android.app.mobilemovies.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -47,7 +46,6 @@ public class MovieReviewsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_movie_reviews, menu);
-
         return true;
     }
 
